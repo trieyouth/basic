e5a3248d163b41043c05013fd98cb0e56b2f6c05
trieyouth <trieyouth@gmail.com>
2015-12-15 16:48:00 +0800

    初始化错误页

---
+++ controllers/CommonController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: youth
+ * Date: 15-12-15
+ * Time: 下午4:45
+ */
+namespace app\controllers;
+use yii\base\Controller;
+
+class CommonController extends Controller{
+
+    public function  actionError(){
+
+    }
+
+}
